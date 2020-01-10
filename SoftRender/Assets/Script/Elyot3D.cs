f990e0ecc618fc61b3ca26bb5980ad571cad2781
hengjie.zhou <zhhj007@126.com>
2020-01-10 10:40:29 +0800

    光栅化

--- SoftRender/Assets/Script/Elyot3D.cs
+++ SoftRender/Assets/Script/Elyot3D.cs
@@ -22,6 +22,13 @@ public class Elyot3D
         b = temp;
     }
     
+    private void SwapVector2(ref Vector2 a,ref Vector2 b)
+    {
+        Vector2 temp = a;
+        a = b;
+        b = temp;
+    }
+    
     public void SetBackgroundColor(Color color)
     {
         for (int i = 0; i < width; i++)
@@ -74,4 +81,67 @@ public class Elyot3D
             } 
         } 
     }
+    
+    //画三角形 扫描线算法
+    public void triangle(Vector2 t0, Vector2 t1, Vector2 t2,Color color) { 
+        
+        // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+        if (t0.y>t1.y) SwapVector2(ref t0, ref t1); 
+        if (t0.y>t2.y) SwapVector2(ref t0, ref t2); 
+        if (t1.y>t2.y) SwapVector2(ref t1, ref t2); 
+        int total_height = (int)t2.y-(int)t0.y; 
+        for (int y=(int)t0.y; y<=(int)t1.y; y++) { 
+            int segment_height = (int)t1.y-(int)t0.y+1; 
+            float alpha = (float)(y-t0.y)/total_height; 
+            float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero 
+            Vector2 A = t0 + (t2-t0)*alpha; 
+            Vector2 B = t0 + (t1-t0)*beta; 
+            if (A.x>B.x) SwapVector2(ref A, ref B); 
+            for (int j=(int)A.x; j<=(int)B.x; j++) { 
+                Pixel(j, y, color); // attention, due to int casts t0.y+i != A.y 
+            } 
+        } 
+        for (int y=(int)t1.y; y<=(int)t2.y; y++) { 
+            int segment_height =  (int)t2.y-(int)t1.y+1; 
+            float alpha = (float)(y-t0.y)/total_height; 
+            float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero 
+            Vector2 A = t0 + (t2-t0)*alpha; 
+            Vector2 B = t1 + (t2-t1)*beta; 
+            if (A.x>B.x) SwapVector2(ref A, ref B); 
+            for (int j=(int)A.x; j<=(int)B.x; j++) { 
+                Pixel(j, y, color); // attention, due to int casts t0.y+i != A.y 
+            } 
+        } 
+    }
+    
+    Vector3 barycentric(Vector2[] pts, Vector2 P) { 
+        Vector3 u = Vector3.Cross(new Vector3(pts[2][0]-pts[0][0], pts[1][0]-pts[0][0], pts[0][0]-P[0]),new Vector3(pts[2][1]-pts[0][1], pts[1][1]-pts[0][1], pts[0][1]-P[1]));
+        /* `pts` and `P` has integer value as coordinates
+           so `abs(u[2])` < 1 means `u[2]` is 0, that means
+           triangle is degenerate, in this case return something with negative coordinates */
+        if (Mathf.Abs(u[2])<1) return new Vector3(-1,1,1);
+        return new Vector3(1.0f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
+    }
+    
+    public void triangle2(Vector2[] pts,Color color) { 
+        Vector2 boxmin = new Vector2(width-1,height - 1);
+        Vector2 boxmax = new Vector2(0,0);
+        Vector2 clamp = new Vector2(width-1,height - 1);
+        
+        for (int i=0; i<3; i++) { 
+            for (int j=0; j<2; j++) { 
+                boxmin[j] = Mathf.Max(0,Mathf.Min(boxmin[j], pts[i][j])); 
+                boxmax[j] = Mathf.Min(clamp[j], Mathf.Max(boxmax[j], pts[i][j])); 
+            } 
+        } 
+        Vector2 P; 
+        for (P.x=boxmin.x; P.x<=boxmax.x; P.x++) { 
+            for (P.y=boxmin.y; P.y<=boxmax.y; P.y++) { 
+                Vector3 bc_screen  = barycentric(pts, P); 
+                if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue; 
+                Pixel((int)P.x, (int)P.y, color); 
+            } 
+        } 
+    } 
+    
 }
--- SoftRender/Assets/Script/Main.cs
+++ SoftRender/Assets/Script/Main.cs
@@ -27,35 +27,35 @@ public class Main
         List<Vector3> verts = _loadObj.Verts;
         List<int> trans = _loadObj.Triangles;
 
+        Vector3 lightDir = Vector3.forward;
+        
         for (int i = 0; i < trans.Count; i+=3)
         {
             Vector3 v0 = verts[trans[i]];
-            Vector3 v1 =verts[trans[i+1]];
+            Vector3 v1 = verts[trans[i+1]];
+            Vector3 v2 =verts[trans[i + 2]];
+
+            Vector2 scenecoord1 = new Vector2((int)((v0.x +1.0f)*_elyot3D.width/2.0f),(int)((v0.y +1.0f)*_elyot3D.height/2.0f)); 
+            Vector2 scenecoord2 = new Vector2((int)((v1.x +1.0f)*_elyot3D.width/2.0f),(int)((v1.y +1.0f)*_elyot3D.height/2.0f)); 
+            Vector2 scenecoord3 = new Vector2((int)((v2.x +1.0f)*_elyot3D.width/2.0f),(int)((v2.y +1.0f)*_elyot3D.height/2.0f)); 
             
-            int x0 = (int) ((v0.x+1.0f)*_elyot3D.width/2.0f); 
-            int y0 = (int) ((v0.y+1.0f)*_elyot3D.height/2.0f); 
-            int x1 = (int) ((v1.x+1.0f)*_elyot3D.width/2.0f); 
-            int y1 = (int) ((v1.y+1.0f)*_elyot3D.height/2.0f); 
-            _elyot3D.line(x0, y0, x1, y1,Color.white); 
+            Vector3 n = Vector3.Cross(v1 - v0, v2 - v1);
+            n = n.normalized;
+
+            float intensity = Vector3.Dot(n, lightDir);
+            intensity = Mathf.Abs(intensity);
+            //_elyot3D.triangle2(new Vector2[] {scenecoord1, scenecoord2, scenecoord3},
+            //    new Color(intensity, intensity, intensity, 1));
             
-            Vector3 v2 = verts[trans[i+1]];
-            Vector3 v3 =verts[trans[i+2]];
-            
-            x0 = (int) ((v2.x+1.0f)*_elyot3D.width/2.0f); 
-            y0 = (int) ((v2.y+1.0f)*_elyot3D.height/2.0f); 
-            x1 = (int) ((v3.x+1.0f)*_elyot3D.width/2.0f); 
-            y1 = (int) ((v3.y+1.0f)*_elyot3D.height/2.0f); 
-            _elyot3D.line(x0, y0, x1, y1,Color.white); 
-            
-            Vector3 v4 =verts[trans[i + 2]];
-            Vector3 v5 = verts[trans[i]];
-            
-            x0 = (int) ((v4.x+1.0f)*_elyot3D.width/2.0f); 
-            y0 = (int) ((v4.y+1.0f)*_elyot3D.height/2.0f); 
-            x1 = (int) ((v5.x+1.0f)*_elyot3D.width/2.0f); 
-            y1 = (int) ((v5.y+1.0f)*_elyot3D.height/2.0f); 
-            _elyot3D.line(x0, y0, x1, y1,Color.white); 
+            _elyot3D.triangle(scenecoord1, scenecoord2, scenecoord3,new Color(intensity,intensity,intensity,1)); 
         }
+
+//        Vector2[] t0 = {new Vector2(10, 70),   new Vector2(50, 160),  new Vector2(70, 80)}; 
+//        Vector2[] t1 = {new Vector2(180, 50),  new Vector2(150, 1),   new Vector2(70, 180)}; 
+//        Vector2[] t2 = {new Vector2(180, 150), new Vector2(140, 300), new Vector2(200, 180)}; 
+//        _elyot3D.triangle(t0[0], t0[1], t0[2],Color.red); 
+//        _elyot3D.triangle(t1[0], t1[1], t1[2],Color.white); 
+//        _elyot3D.triangle(t2[0], t2[1], t2[2],Color.green);
         
         _elyot3D.Apply();
     }
